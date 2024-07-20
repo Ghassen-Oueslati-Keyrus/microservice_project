c1a8f0d22d6e45b0263ee87e499ce2c1ad0114a9
Oueslati-Ghassen <ghassen.oueslati@keyrus.com>
2024-07-20 01:42:14 +0100

    remove old ms

--- Commande-back/src/main/java/com/example/springcrudmongodb/repositories/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springcrudmongodb.repositories;
-
-import com.example.springcrudmongodb.entities.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderRepository extends MongoRepository<Order, String> {
-    // Custom query methods can be added here if needed
-}
